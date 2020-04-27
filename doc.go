763ac4bd3b6f3a966eb905aab4e5f490eb87ca69
Hedzr Yeh <hedzrz@gmail.com>
2020-04-27 09:25:02 +0800

    an example app for cmdr and nt service
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2019 Hedzr Yeh.
+ */
+
+package cmdr
+
+const (
+	// AppName const
+	AppName = "cmdr"
+	// Version const
+	Version = "1.6.7"
+	// VersionInt const
+	VersionInt = 0x010607
+)
