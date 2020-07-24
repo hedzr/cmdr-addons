31a17e70ee29bd8e0a1b8340fecc8a8283674e88
Hedzr Yeh <hedzrz@gmail.com>
2020-07-24 14:20:48 +0800

    update dep and bump to v1.0.17
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.0.16"
+	Version = "1.0.17"
 	// VersionInt const
-	VersionInt = 0x010010
+	VersionInt = 0x010011
 )
