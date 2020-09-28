82f6bd0067c9768f971e582390dcc6c934757952
Hedzr Yeh <hedzrz@gmail.com>
2020-09-28 19:42:31 +0800

    bump to v1.7.25 with new feature: loading command definition from config file
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.7.21"
+	Version = "1.7.25"
 	// VersionInt const
-	VersionInt = 0x010715
+	VersionInt = 0x010719
 )
