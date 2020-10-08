8532a451cdef68b2171fd981a03858d15592252a
Hedzr Yeh <hedzrz@gmail.com>
2020-10-08 19:34:24 +0800

    update deps and release as v1.7.30

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.7.28"
+	Version = "1.7.30"
 	// VersionInt const
-	VersionInt = 0x01071c
+	VersionInt = 0x01071e
 )
