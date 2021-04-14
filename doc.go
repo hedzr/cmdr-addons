ada8385da96733542410e708d72e184637bf16a2
Hedzr Yeh <hedzrz@gmail.com>
2021-04-14 23:34:49 +0800

    bump to v1.7.46

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.7.45"
+	Version = "1.7.46"
 	// VersionInt const
-	VersionInt = 0x01072d
+	VersionInt = 0x01072e
 )
