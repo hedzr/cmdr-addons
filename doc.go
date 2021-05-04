f3a70724c38d63dae3fe8e2e5c3b946a3086a861
Hedzr Yeh <hedzrz@gmail.com>
2021-05-04 19:11:24 +0800

    bump to v1.8.6 for emergency patching.

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.8.4"
+	Version = "1.8.6"
 	// VersionInt const
-	VersionInt = 0x010804
+	VersionInt = 0x010806
 )
