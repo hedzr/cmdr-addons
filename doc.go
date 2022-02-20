f4433bfe5ea13daf1e3d809fe5f38c56cf2d46c7
Hedzr Yeh <hedzrz@gmail.com>
2022-02-20 18:45:36 +0800

    bump to v1.10.27

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.10.24"
+	Version = "1.10.27"
 	// VersionInt const
-	VersionInt = 0x010a18
+	VersionInt = 0x010a1b
 )
