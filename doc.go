b3909a5b8217ac765f3481471d96f9fac9f11a93
Hedzr Yeh <hedzrz@gmail.com>
2023-02-21 14:30:00 +0800

    bump to v1.11.9
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr-addons"
 	// Version const
-	Version = "1.11.8"
+	Version = "1.11.9"
 	// VersionInt const
-	VersionInt = 0x010b08
+	VersionInt = 0x010b09
 )
