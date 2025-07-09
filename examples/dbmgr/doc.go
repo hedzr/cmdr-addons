0144fcb86d304c9dae683827a7efb71ea13b7cab
Hedzr Yeh <hedzrz@gmail.com>
2025-07-09 09:26:28 +0800

    add examples/dbmgr

---
+++ examples/dbmgr/doc.go
@@ -0,0 +1,7 @@
+package main
+
+const AppNameExample = "small" // appName for the current demo app
+
+const appName = "service" // appName of hedzr/is package
+const version = "v0.7.6"  // version of hedzr/is package | update it while bumping hedzr/is' version
+const Version = version
