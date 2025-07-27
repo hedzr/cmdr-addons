08591888c609e360df118a711258d7cbf8be7545
Hedzr Yeh <hedzrz@gmail.com>
2025-07-27 16:24:04 +0800

    bump to v2.0.11
      - upgrade deps
      - [pgsqlogger] improved `New` - added opts args and `WithTableName(name) Opt`

--- examples/small/doc.go
+++ examples/small/doc.go
@@ -3,5 +3,5 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "cmdr-addons" // appName of hedzr/cmdr-loaders package
-const version = "v2.0.8"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+const version = "v2.0.11"     // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
 const Version = version
