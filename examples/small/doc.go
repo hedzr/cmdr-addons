605bcacd3893ac2ba169a055f2bc7eac0f78dcb4
Hedzr Yeh <hedzrz@gmail.com>
2026-01-24 20:58:17 +0800

    bump to v2.0.27 / [pgsqlogger] / [service]   - upgrade cmdr to v2.1.67   - security patch

--- examples/small/doc.go
+++ examples/small/doc.go
@@ -3,5 +3,5 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "cmdr-addons" // appName of hedzr/cmdr-loaders package
-const version = "v2.0.26"     // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+const version = "v2.0.27"     // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
 const Version = version
