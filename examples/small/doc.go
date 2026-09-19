27787dcfbec3c2580c3d844ec7323f798b7d476c
Hedzr Yeh <hedzrz@gmail.com>
2026-09-20 07:55:05 +0800

    bump to v2.2.9 / [pgsqlogger] / [service]   - upgrade cmdr to v2.2.9   - keep the version same with cmdr.v2   - upgrade to go toolchain 1.26

--- examples/small/doc.go
+++ examples/small/doc.go
@@ -4,6 +4,6 @@ const AppNameExample = "small" // appName for the current demo app
 
 const (
 	appName = "cmdr-addons" // appName of hedzr/cmdr-loaders package
-	version = "v2.2.3"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+	version = "v2.2.9"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
 	Version = version
 )
